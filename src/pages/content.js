f3b762e90006794ed7661fba98e8089782327fb8
AREZKI BAZIZI <baziziarezki@gmail.com>
2023-11-15 19:36:54 +0100

    ADD

---
+++ src/pages/content.js
@@ -0,0 +1,151 @@
+// Personal Information --> #hero section
+export const name = "Arezki Bazizi"
+export const designation = "Developpeur Web"
+export const location = "France"
+export const website = "https://github.com/ArezkiBazizi/ArezkiBazizi"
+
+// About
+export const Bio = "Bienvenue dans Portfolio, cette petite brèche spatio-temporelle qui vous donne un aperçu sur mon univers créatif. Passioné par les technologies du numérique, et curieux de nature, j'ai aqcuis par le biais de mes efforts personnels et de mon encadrement académique des connaisances dans divers domaine informatique : Dev Web Full-Stack, Programataion Orienté Object, Data Science et Inteligence Artificielle.. "
+
+// Work Experience --> #work section
+export const stages = [
+	{
+		from: "fév 2023",
+		to: "Mars 2023",
+		role: "Prédiction de la résiliation de contrat des clients",
+		org: "Ooredoo Algérie",
+		org_url: "https://www.ooredoo.dz/fr/",
+		location: "Bejaia, Algerie",
+		skills: "Data Mining : Collecter, analyser et préparer les données des clients. Construction du modèle d'apprentissage : Knn et régression logistique. Prédiction et Conclusion : Comparer les performances des deux méthodes en termes d'exactitude."
+	},
+
+	{
+		from: "Déc 2022",
+		to: "Jan 2023",
+		role: "Architecte bases de données",
+		org: "SONATRACH",
+		org_url: "https://sonatrach.com/",
+		location: "Bejaia, Algeie",
+		skills: " L'étude des normes, standards et procédures du service HSE, Étude du SI existant au sein du département, Conception et l'implémentation d'une solution base de données pour la gestion et la maintenance du service HSE, Implémentation d'une interface graphique avec Python"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "Application Web E-Commerce Full-Stack",
+		description: "Développement d'une application de commerce full-stack comme projet de fin de cycle",
+		url: "https://github.com/ArezkiBazizi/FullStack-E-Commerce",
+		tech: "Front-end : JavaScript, HTML, CSS, Bootstrap; Back-end : PHP, MYSQL",
+	},
+	{
+		title: "Site Vitrine Commerce Local",
+		description: "Site web statique pour le profit d'un commerce local",
+		url: "https://bfsup.netlify.app/",
+		tech: "HTML, CSS, JS",
+	},
+]
+
+// Education --> #education section
+export const formation = [
+	{
+		from: "2023",
+		to: "2024",
+		institute: "Université de Haute-Alsace",
+		course: "Licence informatique option MIAGE",
+		location: "Mulhouse, France"
+	},
+	{
+		from: "2022",
+		to: "2023",
+		institute: "Université de Abderahmane Mira",
+		course: "Master informatioque : Systèmes d'Informations Avancées",
+		location: "Bejaia, Algerie"
+	},
+	{
+		from: "2019",
+		to: "2022",
+		institute: "Université de Abderahmane Mira",
+		course: "Licence informatioque option Système d'Informations",
+		location: "Bejaia, Algerie"
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		title: "Email",
+		description: "baziziarezki@gmail.com",
+	},
+	{
+		title: "Whatapp",
+		description: "+33769614503",
+	},
+	{
+		title : "Profil LinkedIn",
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/arezki-bazizi-recherche-stage-informatique-d%C3%A9veloppement-web-alternance-/",
+	},
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+
+		issued: "2023",
+		title: "Introduction to Cybersecurity",
+		org: "Cisco",
+		url: "https://www.credly.com/badges/bcabcfcc-9038-42f7-8f86-d3ad1475b220/linked_in_profile",
+		description: "Cisco Certification",
+	},
+	{
+		issued: "2022",
+		title: "Full-Stack Web Development with React",
+		org: "The Hong Kong University of Science and Technology",
+		url: "https://coursera.org/verify/specialization/FY4UDE3Q825G",
+		description: "Coursera Certification",
+	},
+	{
+		issued: "2022",
+		title: "Server-side Development with NodeJS, Express and MongoDB",
+		org: "The Hong Kong University of Science and Technology",
+		url: "https://coursera.org/verify/5AVUCSATXG2J",
+		description: "Coursera Certification",
+	},
+
+	{
+		issued: "2022",
+		title: "Cloud Computing",
+		org: "Huawei",
+		url: "01010080249808459091347973",
+		description: "Huawei Certification",
+	},
+	
+	{
+		issued: "2021",
+		title: "Java Programming: Solving Problems with Software",
+		org: "Duke University",
+		url: "https://www.coursera.org/account/accomplishments/verify/XUYTQZSSEWFB",
+		description: "Coursera Certification",
+	},
+
+	{
+		issued: "2021",
+		title: "Managing Big Data with Mysql",
+		org: "Duke University",
+		url: "https://www.coursera.org/account/accomplishments/verify/NBAK4XGPK69F",
+		description: "Coursera Certification",
+	},
+	{
+		issued: "2022",
+		title: "Crash Course On python",
+		org: "Google",
+		url: "https://www.coursera.org/account/accomplishments/verify/PBSNSPBE6WXG",
+		description: "Coursera Certification",
+
+	}
+]
+
+// Blogs --> #blogs section
+
